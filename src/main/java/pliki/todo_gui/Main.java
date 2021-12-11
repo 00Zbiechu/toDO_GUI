c0789a8f74d025b842b248b6f3992a53a1ea2a0f
Mateusz Zbiewski <00Zbiewski@gmail.com>
2021-12-11 20:49:55 +0100

    Tworznie pierwszej struktury plikow

--- src/main/java/pliki/todo_gui/Main.java
+++ src/main/java/pliki/todo_gui/Main.java
@@ -7,10 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View1.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -18,6 +20,13 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+
         launch();
+        
+
+
+
+
     }
+
 }
