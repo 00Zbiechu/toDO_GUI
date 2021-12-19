010fef8f9a89f5dc376f2f399d0164a8153a417b
Mateusz Zbiewski <00Zbiewski@gmail.com>
2021-12-19 18:56:45 +0100

    Dodanie nazwy usera do następnego kontrolera i sceny

---
+++ src/main/java/pliki/todo_gui/UserController.java
@@ -0,0 +1,31 @@
+package pliki.todo_gui;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchToScene2(ActionEvent e) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("View2.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+}
