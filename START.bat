5d07fdfaef8f481e05516e0b3fb5b7cba5b98ddf
Windows <00Zbiewski@gmail.com>
2022-02-27 17:14:23 +0100

    Dodanie skryptów uruchomieniowych .bat, .sh v4

---
+++ START.bat
@@ -0,0 +1,1 @@
+java --module-path javafx-sdk-17.0.2/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar toDO_GUI.jar
---
+++ START.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Podaj sciezke do katalogu lib JavaFX"
+read path
+java --module-path $path --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar toDO_GUI.jar
