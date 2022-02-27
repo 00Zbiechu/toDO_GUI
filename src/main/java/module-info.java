b7cbd5e7ed1e773af5e80b7e330624fdcd1a81ff
Mateusz Zbiewski <00Zbiewski@gmail.com>
2022-02-27 18:13:43 +0100

    Modyfikacja plików uruchomieniowych v5

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module pliki.todo_gui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    opens pliki.todo_gui to javafx.fxml;
-    exports pliki.todo_gui;
-}
