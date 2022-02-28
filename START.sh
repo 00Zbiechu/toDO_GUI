e74eafd600c85047a20d362fb5c237e7c3aba8f1
Mateusz Zbiewski <00Zbiewski@gmail.com>
2022-02-28 22:20:00 +0100

    Zmiana wersji JDK na 11

--- START.sh
+++ START.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Podaj sciezke do katalogu lib JavaFX"
 read path
-java --module-path $path --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar toDO_GUI.jar
+java --module-path $path --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar toDO_GUI_v2.jar
